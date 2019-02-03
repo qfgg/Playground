0710b7275a9db89f670e26887512af262d0ab3ae
zhurui66 <cdzhurui8@jd.com>
2019-02-03 17:19:23 +0800

    Optimize combination

--- combination.js
+++ combination.js
@@ -1,8 +1,10 @@
 var matrix = [['a', 'b'], ['c', 'd', 'e'], ['f', 'g', 'h', 'i']];
-// "fca", "fcb", "fda", "fdb", "fea", "feb",
-// "gca", "gcb", "gda", "gdb", "gea", "geb",
-// "hca", "hcb", "hda", "hdb", "hea", "heb",
-// "ica", "icb", "ida", "idb", "iea", "ieb"
+// "acf", "acg", "ach", "aci",
+// "adf", "adg", "adh", "adi",
+// "aef", "aeg", "aeh", "aei",
+// "bcf", "bcg", "bch", "bci",
+// "bdf", "bdg", "bdh", "bdi",
+// "bef", "beg", "beh", "bei"
 // 递归
 function cmb(mat) {
     if (!mat || Object.prototype.toString.call(mat) !== '[object Array]') {
@@ -31,15 +33,17 @@ function cmb2(mat) {
     var total = mat.length;
     while(total > 1){
         var tmp = [];
-        for(var i = 0, l1 = list[total - 1].length; i < l1; i++) {
-            for (var j = 0, l2 = list[total - 2].length; j < l2; j++) {
-                tmp.push(list[total - 1][i] + list[total - 2][j]);
+        for(var i = 0, l1 = list[0].length; i < l1; i++) {
+            for (var j = 0, l2 = list[1].length; j < l2; j++) {
+                tmp.push(list[0][i] + list[1][j]);
             }
         }
-        list.splice(total - 2, 2, tmp);
+        list.splice(0, 2, tmp);
         total--;
     }
     return list[0];
 }
-var res = cmb2(matrix);
+var res = cmb(matrix);
+var res2 = cmb2(matrix);
 console.log(res);
+console.log(res2);
