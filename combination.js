7c4ffb5faf270d113d105c529923cf6a2c7320bc
zhurui66 <cdzhurui8@jd.com>
2019-02-03 17:10:58 +0800

    New solution to combination

--- combination.js
+++ combination.js
@@ -1,5 +1,9 @@
-var matrix = [['a', 'b'],['c', 'd'], ['e', 'f']];
-
+var matrix = [['a', 'b'], ['c', 'd', 'e'], ['f', 'g', 'h', 'i']];
+// "fca", "fcb", "fda", "fdb", "fea", "feb",
+// "gca", "gcb", "gda", "gdb", "gea", "geb",
+// "hca", "hcb", "hda", "hdb", "hea", "heb",
+// "ica", "icb", "ida", "idb", "iea", "ieb"
+// 递归
 function cmb(mat) {
     if (!mat || Object.prototype.toString.call(mat) !== '[object Array]') {
         return [];
@@ -18,6 +22,24 @@ function cmb(mat) {
         return list;
     }
 }
-
-var res = cmb(matrix);
+// 非递归
+function cmb2(mat) {
+    if (!mat || Object.prototype.toString.call(mat) !== '[object Array]') {
+        return [];
+    }
+    var list = mat.slice();
+    var total = mat.length;
+    while(total > 1){
+        var tmp = [];
+        for(var i = 0, l1 = list[total - 1].length; i < l1; i++) {
+            for (var j = 0, l2 = list[total - 2].length; j < l2; j++) {
+                tmp.push(list[total - 1][i] + list[total - 2][j]);
+            }
+        }
+        list.splice(total - 2, 2, tmp);
+        total--;
+    }
+    return list[0];
+}
+var res = cmb2(matrix);
 console.log(res);
