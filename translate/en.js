a0843322ff7803dc1d0c542e516dcae9bd57ae61
zhurui <zhurui@navercorp.com>
2019-04-08 21:04:38 +0800

    Add translate

---
+++ translate/en.js
@@ -0,0 +1,97 @@
+var textEn = function() {/*
+Catastrophes  Weather
+In 1674, a mysterious storm devastated an entire city
+The storm only lasted minutes, but it caused enormous damage to the city of Utrecht. Now it seems there may be an explanation
+
+By Colin Barras
+10 April 2017
+It took barely 15 minutes. An intense and powerful – but brief – storm swept across north-west Europe one summer's evening almost 350 years ago. It left a social and architectural imprint on the city of Utrecht that is felt to this day.
+
+A new study offers the best explanation yet for exactly what form that devastating storm took.
+
+The late 17th Century was a challenging time for the Netherlands. The Dutch found themselves in a war against several foreign forces, including the French and English.
+
+Boats in Amsterdam were plucked from the water and transported over several fields before crashing down
+
+The year 1672 in particular is an infamous one in Dutch history. It is known as the "Disaster Year", because it marked an invasion of the Dutch Republic by forces from France, England and regions of what is now Germany.
+
+"The French took all the money there was. Utrecht was left completely broke," says Gerard van der Schrier at the Royal Netherlands Meteorological Institute in De Bilt.
+
+The city was left uniquely ill-prepared for a major natural disaster. Which is unfortunate, given that two years later exactly such a disaster struck.
+
+Wednesday 1 August 1674 was a typical warm and humid summer's day in northwest Europe. Towards the end of the day, thunderstorms developed – again, not atypical weather for a humid summer's day.
+
+But the storms escalated and escalated. By about 6pm a devastating tempest had begun tearing its way north across the region, leaving a wake of extensive destruction in its wake.
+
+One church originally had two towers. It still does, but the tops were blown off in the storm
+
+Dozens of eyewitness accounts from the time capture the scene.
+
+According to one newspaper: "In Brussels, hailstones fell which were as large as marbles, many trees were removed from the Earth, but also many house facades were overthrown… In Strassbourg, hailstones fell as large as baby's heads."
+
+A Dutch merchant named Gerrit Jansz Kooch penned a poem in the aftermath, describing how boats in Amsterdam were plucked from the water and transported over several fields before crashing down. Kooch also relates the story of one farmer who could not recognise his own fields after the storm had passed. The trees that bordered the land were gone, and so were the church towers from the nearby villages.
+
+Utrecht seems to have borne the brunt of the damage. Even to this day the effects of the storm are visible.
+
+"The Dom cathedral is the most obvious sign of the storm," says van der Schrier. The central part of the building was flattened by the tempest. "There is now a huge gap between the tower and the choir."
+
+They describe how it only took 15 minutes for it to pass
+
+Many more of Utrecht's churches were damaged too. "Some have no towers, which is strange. One church originally had two towers. It still does, but the tops were blown off in the storm so the two towers are not similar anymore."
+
+But the eyewitness accounts also make two more points very clear.
+
+One is that the damage was unusually localised. Buildings in one street may have been flattened while those in the next escaped any significant damage.
+
+The other point is that the storm was very short-lived.
+
+"There are several accounts of farmers who took cover as they saw this frontal system coming, which would have been very black," says van der Schrier. "They describe how it only took 15 minutes for it to pass. Then, when they stood up, the sky was completely clear again."
+
+A tornado is too localised and too confined to account for all of the damage seen in Utrecht
+
+Back in 1980, meteorologists concluded that the damage in Utrecht must have been the result of a tornado, which can tear through an area quickly and leave a localised path of destruction behind.
+
+But van der Schrier and his colleague, Rob Groenland, think there is now a better explanation for the storm of 1674. They detail their research in the journal Natural Hazards and Earth System Sciences.
+
+The problem is that a tornado is too localised and too confined to account for all of the damage seen in Utrecht. It might account for destruction in one particular district of the city, but it cannot explain why there were several hotspots of strong activity across a wide region of northwest Europe.
+
+But a particular form of storm system called a "bow echo" can.
+
+Bow echoes are relatively new to meteorologists. The term was first used about 40 years ago, and as weather radar has become more widely used, bow echoes have become easier to identify and study.
+
+Small differences in wind strength can generate small whirlwinds
+
+Bow echoes are broad, arc-shaped weather fronts – bow-shaped, in fact – which can be tens to hundreds of kilometres long.
+
+They pass over an area in a matter of a few hours at most, and they are associated with strong and localised gusts of wind, called straight-line winds.
+
+These winds are related to downdrafts – winds that move vertically towards Earth's surface at great speed. In the context of a bow echo, the winds flow parallel to Earth's surface in the direction of the frontal system once they reach the ground.
+
+"We have been able to reconstruct the shape of the front by using historical observations," says van der Schrier.
+
+For instance, eyewitness accounts suggest the central part of the front passed over Utrecht slightly ahead of the flanks, which fits with the idea that the front was slightly curved into a bow-shaped arc.
+
+By chance this storm happens to be one of those rare events and it's well documented
+
+Within straight-line winds, small differences in wind strength can generate small whirlwinds. In line with this, there is evidence of these whirlwinds in Utrecht.
+
+Paintings of the destruction show that the nave of the Dom cathedral and some church towers toppled in a northerly direction – the way that the front was travelling – but others fell to the west, perpendicular to the direction of the front. They can only have been brought down by whirlwinds producing strong westerly gusts.
+
+"So far, everything we have from the eyewitness accounts is in line with the hypothesis that this was a bow echo," says van der Schrier.
+
+This sort of work has the potential to be valuable to the authorities in the Netherlands, says van der Schrier. They are keen to ensure that buildings and other structures are strong enough to withstand rare but severe storms.
+
+'Utrechter' became synonymous with homosexuals – that's still the case today – and it relates to this storm
+
+"We don't have measurements of these extreme events – we only have 100 years of measurements – but by chance this storm happens to be one of those rare events and it's well documented."
+
+Understanding exactly how the storm of 1674 developed, and carefully documenting the resulting damage, could prove useful for designing and building structures that are sufficiently robust against similar storm events.
+
+But beyond that, there is a degree to which this sort of scientific investigation is interesting just for curiosity's sake. The storm is an important, if sometimes overlooked, chapter in the history of Utrecht.
+
+Because the storm came just two years after the "Disaster Year", there was little money to pay for clear-up operations. The Dom cathedral remained in ruins for decades. "The ruins became a place where homosexuals could meet," says van der Schrier.
+
+More than 50 years later the Dutch began persecuting such homosexual activities. The trials began in Utrecht at the behest of Josua Wils, the man in charge of the sacristy of the cathedral.
+
+"So then homosexual behaviour was described as coming from the city of Utrecht," says van der Schrier. The Dutch word "Utrechtenaar" became synonymous with homosexuality. "That's still the case today and it relates to this storm. Although not many people realise this."
+.*/};
