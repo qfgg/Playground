829cec7ee73bd727b3f5d5309c7a6f05b0f68fda
zhurui66 <cdzhurui8@jd.com>
2019-06-03 18:36:18 +0800

    Upgrade subtitle project

--- lyric/sub.js
+++ lyric/sub.js
@@ -1,5 +1,32 @@
 var text = function() {/*
 
+1
+00:00:00,000 --> 00:00:06,000
+【海盗美剧 友情制作】
+-== http://www.hdmeiju.com ==-
+新浪微博：@海盗美剧
+
+2
+00:00:07,000 --> 00:00:13,000
+《老友记》蓝光版 第01季 第01集
+
+3
+00:00:14,000 --> 00:00:20,000
+字幕来源：射手网      调校制作：寒 枫
+
+4
+00:00:21,000 --> 00:00:26,000
+字幕编辑：菲衣粒粒 林奇先生
+
+5
+00:00:49,960 --> 00:00:52,760
+没什么好说的 他不过是我的同事
+There's nothing to tell. It's just some guy I work with.
+
+6
+00:00:52,960 --> 00:00:55,130
+少来了 和你交往的男人
+Come on. You're going out with a guy.It's an amazing accomplishment and one we cannot achieve without generous support from individuals, corporations, and other social organizations. Visit pacificsciencecenter.org to find various ways you can support Pacific Science Center.
 
 
 */};
