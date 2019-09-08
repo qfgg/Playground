dfdc697ce583f6e701b0e849968f14204ead6f19
zhurui <zhurui@navercorp.com>
2019-09-08 19:18:52 +0800

    Update

--- index.js
+++ index.js
@@ -35,39 +35,39 @@ window.onload = function() {
 //     }
 //     console.log(arr);
 // }
-/*
-function swap(arr, i, j) {
-    var tmp = arr[i];
-    arr[i] = arr[j];
-    arr[j] = tmp;
-}
-function qsort(arr, s, e, fn) {
-    if (s >= e) {
-        return;
-    }
-    var i = s;
-    var j = e;
-    while(j > i) {
-        while(j > i && ((fn && fn(arr[j], arr[s]) >=0) || (!fn && arr[j] >= arr[s]))) {
-            j--;
-        }
-        while(j > i && ((fn && fn(arr[i], arr[s]) <=0) || (!fn && arr[i] <= arr[s]))) {
-            i++
-        }
-        if (i !== j) {
-            swap(arr, i, j);
-        }
-    }
-    if (s !== j) {
-        swap(arr, s, j);
-    }
-    qsort(arr, s, j - 1, fn);
-    qsort(arr, j + 1, e, fn);
-}
-function quicksort(arr, fn) {
-    qsort(arr, 0, arr.length - 1, fn);
-}
-*//*
+
+// function swap(arr, i, j) {
+//     var tmp = arr[i];
+//     arr[i] = arr[j];
+//     arr[j] = tmp;
+// }
+// function qsort(arr, s, e, fn) {
+//     if (s >= e) {
+//         return;
+//     }
+//     var i = s;
+//     var j = e;
+//     while(j > i) {
+//         while(j > i && ((fn && fn(arr[j], arr[s]) >=0) || (!fn && arr[j] >= arr[s]))) {
+//             j--;
+//         }
+//         while(j > i && ((fn && fn(arr[i], arr[s]) <=0) || (!fn && arr[i] <= arr[s]))) {
+//             i++
+//         }
+//         if (i !== j) {
+//             swap(arr, i, j);
+//         }
+//     }
+//     if (s !== j) {
+//         swap(arr, s, j);
+//     }
+//     qsort(arr, s, j - 1, fn);
+//     qsort(arr, j + 1, e, fn);
+// }
+// function quicksort(arr, fn) {
+//     qsort(arr, 0, arr.length - 1, fn);
+// }
+
 function TreeNode(val) {
     this.val = val;
     this.left = this.right = null;
@@ -94,77 +94,75 @@ function createBinary(arr) {
     return tmp[0];
 }
 
-var tree = createBinary([3,9,20,1,2,15,7]);
-*/
-function insertHeap(heap, n, fn = function(a, b) { return a - b; }) {
-    heap.push(n);
+// function insertHeap(heap, n, fn = function(a, b) { return a - b; }) {
+//     heap.push(n);
 
-    goUp(heap, heap.length - 1, fn);
-}
+//     goUp(heap, heap.length - 1, fn);
+// }
 
-function goUp(heap, i, fn) {
-    var parent = Math.floor((i - 1) / 2);
+// function goUp(heap, i, fn) {
+//     var parent = Math.floor((i - 1) / 2);
 
-    while (parent >= 0) {
-        if (fn(heap[parent], heap[i]) > 0) {
-            var tmp = heap[parent];
-            heap[parent] = heap[i];
-            heap[i] = tmp;
-        }
-        i = parent;
-        parent = Math.floor((i - 1) / 2);
-    }
-}
+//     while (parent >= 0) {
+//         if (fn(heap[parent], heap[i]) > 0) {
+//             var tmp = heap[parent];
+//             heap[parent] = heap[i];
+//             heap[i] = tmp;
+//         }
+//         i = parent;
+//         parent = Math.floor((i - 1) / 2);
+//     }
+// }
 
-function goDown(nums, i, max, fn) {
-    var tmp, swapIdx;
-    var leftChild = 2 * i + 1;
-    var rightChild = 2 * i + 2;
+// function goDown(nums, i, max, fn) {
+//     var tmp, swapIdx;
+//     var leftChild = 2 * i + 1;
+//     var rightChild = 2 * i + 2;
 
-    while (leftChild <= max) {
-        if (rightChild > max) {
-            if (fn(nums[leftChild], nums[i]) < 0) {
-                swapIdx = leftChild;
-            }
-        } else {
-            if (fn(nums[leftChild], nums[i]) < 0 && fn(nums[rightChild], nums[i]) < 0) {
-                swapIdx = fn(nums[leftChild], nums[rightChild]) < 0 ?
-                    leftChild : rightChild;
-            } else if (fn(nums[leftChild], nums[i]) < 0) {
-                swapIdx = leftChild;
-            } else if(fn(nums[rightChild], nums[i]) < 0) {
-                swapIdx = rightChild;
-            }
-        }
-        if (swapIdx !== undefined) {
-            tmp = nums[swapIdx];
-            nums[swapIdx] = nums[i];
-            nums[i] = tmp;
-            i = swapIdx;
-            leftChild = 2 * i + 1;
-            rightChild = 2 * i + 2;
-            swapIdx = undefined;
-        } else  {
-            break;
-        }
-    }
-}
+//     while (leftChild <= max) {
+//         if (rightChild > max) {
+//             if (fn(nums[leftChild], nums[i]) < 0) {
+//                 swapIdx = leftChild;
+//             }
+//         } else {
+//             if (fn(nums[leftChild], nums[i]) < 0 && fn(nums[rightChild], nums[i]) < 0) {
+//                 swapIdx = fn(nums[leftChild], nums[rightChild]) < 0 ?
+//                     leftChild : rightChild;
+//             } else if (fn(nums[leftChild], nums[i]) < 0) {
+//                 swapIdx = leftChild;
+//             } else if(fn(nums[rightChild], nums[i]) < 0) {
+//                 swapIdx = rightChild;
+//             }
+//         }
+//         if (swapIdx !== undefined) {
+//             tmp = nums[swapIdx];
+//             nums[swapIdx] = nums[i];
+//             nums[i] = tmp;
+//             i = swapIdx;
+//             leftChild = 2 * i + 1;
+//             rightChild = 2 * i + 2;
+//             swapIdx = undefined;
+//         } else  {
+//             break;
+//         }
+//     }
+// }
 
-function popHeap(nums, fn = function(a, b) { return a - b; }) {
-    if (nums.length === 1) {
-        return nums.pop();
-    }
+// function popHeap(nums, fn = function(a, b) { return a - b; }) {
+//     if (nums.length === 1) {
+//         return nums.pop();
+//     }
 
-    var r = nums[0];
-    nums[0] = nums.pop();
-    goDown(nums, 0, nums.length - 1, fn);
+//     var r = nums[0];
+//     nums[0] = nums.pop();
+//     goDown(nums, 0, nums.length - 1, fn);
 
-    return r;
-}
+//     return r;
+// }
 
-function cmp (a, b) {
-    return a.localeCompare(b);
-}
+// function cmp (a, b) {
+//     return a.localeCompare(b);
+// }
 
 // var findItinerary = function(tickets) {
 //     var res = [];
@@ -304,4 +302,38 @@ function cmp (a, b) {
 
 // var r = maximalSquare(A);
 // console.log(r);
+
+var serialize = function(root) {
+    var stack = [];
+    var res = '';
+    var cur = root;
+
+    while (stack.length > 0 || cur !== null) {
+        while (cur !== null) {
+            stack.push(cur);
+            cur = cur.left;
+        }
+        cur = stack.pop();
+        if (cur.left === null) {
+            res += '#_';
+        }
+        res = res + cur.val + '_';
+        if (cur.right === null) {
+            res += '#_';
+        }
+        cur = cur.right;
+    }
+
+    return res;
+};
+
+var deserialize = function(data) {
+    
+};
+
+var tree = createBinary([1,2,3,4,5,6,7]);
+var r = serialize(tree);
+console.log(r);
+t = deserialize(r);
+console.log(t);
 };
