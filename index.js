734dfc3f2684263729d658e11e7e721980db83ab
zhurui <zhurui@navercorp.com>
2019-09-10 23:22:37 +0800

    Update

--- index.js
+++ index.js
@@ -78,45 +78,46 @@ window.onload = function() {
 //     return a.localeCompare(b);
 // }
 
-// var findItinerary = function(tickets) {
-//     var res = [];
-//     var map = {};
+var findItinerary = function(tickets) {
+    var res = [];
+    var map = {};
 
-//     for (var i = 0, len = tickets.length; i < len; i++) {
-//         if (map[tickets[i][0]] === undefined) {
-//             map[tickets[i][0]] = [tickets[i][1]];
-//         } else {
-//             insertHeap(map[tickets[i][0]], tickets[i][1], cmp);
-//         }
-//     }
+    for (var i = 0, len = tickets.length; i < len; i++) {
+        if (map[tickets[i][0]] === undefined) {
+            map[tickets[i][0]] = [tickets[i][1]];
+        } else {
+            insertHeap(map[tickets[i][0]], tickets[i][1], cmp);
+        }
+    }
 
-//     search('JFK', 0, len, res, map);
+    search('JFK', 0, len, res, map);
 
-//     return res; 
-// };
+    return res; 
+};
 
-// function search(cur, idx, max, oneway, map) {
-//     if (idx === max) {
-//         oneway.push(cur);
-//         return true;
-//     }
+function search(cur, idx, max, oneway, map) {
+    if (idx === max) {
+        oneway.push(cur);
+        return true;
+    }
 
-//     var next;
-//     for(var i = 0, len = (map[cur] || []).length; i < len; i++) {
-//         oneway.push(cur);
-//         next = popHeap(map[cur], cmp);
-//         if (search(next, idx + 1, max, oneway, map)) {
-//             return true;
-//         }
-//         oneway.pop(cur);
-//         insertHeap(map[cur], next, cmp);
-//     }
-// }
+    var next;
+    for(var i = 0, len = (map[cur] || []).length; i < len; i++) {
+        oneway.push(cur);
+        next = popHeap(map[cur], cmp);
+        if (search(next, idx + 1, max, oneway, map)) {
+            return true;
+        }
+        oneway.pop(cur);
+        insertHeap(map[cur], next, cmp);
+    }
+}
 
 // var tickets = [["AXA","EZE"],["EZE","AUA"],["ADL","JFK"],["ADL","TIA"],["AUA","AXA"],["EZE","TIA"],["EZE","TIA"],["AXA","EZE"],["EZE","ADL"],["ANU","EZE"],["TIA","EZE"],["JFK","ADL"],["AUA","JFK"],["JFK","EZE"],["EZE","ANU"],["ADL","AUA"],["ANU","AXA"],["AXA","ADL"],["AUA","JFK"],["EZE","ADL"],["ANU","TIA"],["AUA","JFK"],["TIA","JFK"],["EZE","AUA"],["AXA","EZE"],["AUA","ANU"],["ADL","AXA"],["EZE","ADL"],["AUA","ANU"],["AXA","EZE"],["TIA","AUA"],["AXA","EZE"],["AUA","SYD"],["ADL","JFK"],["EZE","AUA"],["ADL","ANU"],["AUA","TIA"],["ADL","EZE"],["TIA","JFK"],["AXA","ANU"],["JFK","AXA"],["JFK","ADL"],["ADL","EZE"],["AXA","TIA"],["JFK","AUA"],["ADL","EZE"],["JFK","ADL"],["ADL","AXA"],["TIA","AUA"],["AXA","JFK"],["ADL","AUA"],["TIA","JFK"],["JFK","ADL"],["JFK","ADL"],["ANU","AXA"],["TIA","AXA"],["EZE","JFK"],["EZE","AXA"],["ADL","TIA"],["JFK","AUA"],["TIA","EZE"],["EZE","ADL"],["JFK","ANU"],["TIA","AUA"],["EZE","ADL"],["ADL","JFK"],["ANU","AXA"],["AUA","AXA"],["ANU","EZE"],["ADL","AXA"],["ANU","AXA"],["TIA","ADL"],["JFK","ADL"],["JFK","TIA"],["AUA","ADL"],["AUA","TIA"],["TIA","JFK"],["EZE","JFK"],["AUA","ADL"],["ADL","AUA"],["EZE","ANU"],["ADL","ANU"],["AUA","AXA"],["AXA","TIA"],["AXA","TIA"],["ADL","AXA"],["EZE","AXA"],["AXA","JFK"],["JFK","AUA"],["ANU","ADL"],["AXA","TIA"],["ANU","AUA"],["JFK","EZE"],["AXA","ADL"],["TIA","EZE"],["JFK","AXA"],["AXA","ADL"],["EZE","AUA"],["AXA","ANU"],["ADL","EZE"],["AUA","EZE"]];
-// // var tickets = [["JFK","SFO"],["JFK","ATL"],["SFO","ATL"],["ATL","JFK"],["ATL","SFO"]];
-// var r = findItinerary(tickets);
-// console.log(r);
+var tickets = [["JFK","SFO"],["JFK","ATL"],["SFO","ATL"],["ATL","JFK"],["ATL","SFO"]];
+var r = findItinerary(tickets);
+console.log(r);
+
 // var ladderLength = function(beginWord, endWord, wordList) {
 //     var startIdx = wordList.indexOf(beginWord);
 
@@ -217,9 +218,4 @@ window.onload = function() {
 // var r = maximalSquare(A);
 // console.log(r);
 
-
-
-
-var r = serialize(tree);
-console.log(r);
 };
