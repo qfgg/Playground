0f84008304041c0f124c8695a3ba1e2f59e78bc6
zhurui <zhurui@navercorp.com>
2019-09-12 17:56:12 +0800

    Fix MaxAjax

--- MaxAjax.js
+++ MaxAjax.js
@@ -7,49 +7,50 @@ function MaxAjax(max = 6) {
 	this.taskQueue = [];
 }
 
-MaxAjax.prototype.ajax = function(requestObj) {
+MaxAjax.prototype.ajax = function(requestObj, callback) {
 	var self = this;
+	var top;
 
 	if (this.count < this.max) {
 		this.count++;
 		
-		return new Promise(function(resolve, reject) {
-			axios(requestObj)
-				.then(function (response) {
-					resolve(response);
+		axios(requestObj)
+			.then(function (response) {
+				callback(response);
 
-					self.count--;
+				self.count--;
 
-					if (self.taskQueue.length > 0) {
-						self.ajax(self.taskQueue.shift());
-					}
-				})
-				.catch(function (error) {
-					reject(error);
+				if (self.taskQueue.length > 0) {
+					top = self.taskQueue.shift();
+					self.ajax(top[0], top[1]);
+				}
+			})
+			.catch(function (error) {
+				callback(null, error);
 
-					self.count--;
-					
-					if (self.taskQueue.length > 0) {
-						self.ajax(self.taskQueue.shift());
-					}
-				});
-		});
+				self.count--;
+				
+				if (self.taskQueue.length > 0) {
+					top = self.taskQueue.shift();
+					self.ajax(top[0], top[1]);
+				}
+			});
 	} else {
-		this.taskQueue.push(requestObj);
+		this.taskQueue.push([requestObj, callback]);
 	}
 }
 
-MaxAjax.prototype.post = function(url, params) {
+MaxAjax.prototype.post = function(url, params, callback) {
 	return this.ajax({
 	  	method: 'post',
 	  	url: url,
 	  	data: params
-	});
+	}, callback);
 }
 
-MaxAjax.prototype.get = function(url) {
+MaxAjax.prototype.get = function(url, callback) {
 	return this.ajax({
 	  	method: 'get',
 	  	url: url,
-	});
+	}, callback);
 }
