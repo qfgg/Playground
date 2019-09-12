b02302d0e2147d23465165acdf40f66a56ef430f
zhurui <zhurui@navercorp.com>
2019-09-12 17:37:31 +0800

    MaxAjax

--- MaxAjax.js
+++ MaxAjax.js
@@ -1,18 +1,55 @@
-function AjaxPool(max) {
+function MaxAjax(max = 6) {
 	// max simultaneous ajax
 	this.max = max;
-	this.pool = [];
-}
-
-AjaxPool.prototype.set = function() {}
-
-function MaxAjax(max = 6) {
+	// current number of running ajax requests
+	this.count = 0;
 	// ajax requests to be sent
-	this.taskStack = [];
-	// max request ajax pool for managing simultaneous requests
-	this.ajaxPool = new AjaxPool(max);
+	this.taskQueue = [];
 }
 
-MaxAjax.prototype.ajax = function(ajaxObj, callback) {
-	this.ajaxPool.set(ajaxObj)
+MaxAjax.prototype.ajax = function(requestObj) {
+	var self = this;
+
+	if (this.count < this.max) {
+		this.count++;
+		
+		return new Promise(function(resolve, reject) {
+			axios(requestObj)
+				.then(function (response) {
+					resolve(response);
+
+					self.count--;
+
+					if (self.taskQueue.length > 0) {
+						self.ajax(self.taskQueue.shift());
+					}
+				})
+				.catch(function (error) {
+					reject(error);
+
+					self.count--;
+					
+					if (self.taskQueue.length > 0) {
+						self.ajax(self.taskQueue.shift());
+					}
+				});
+		});
+	} else {
+		this.taskQueue.push(requestObj);
+	}
+}
+
+MaxAjax.prototype.post = function(url, params) {
+	return this.ajax({
+	  	method: 'post',
+	  	url: url,
+	  	data: params
+	});
+}
+
+MaxAjax.prototype.get = function(url) {
+	return this.ajax({
+	  	method: 'get',
+	  	url: url,
+	});
 }
--- index.js
+++ index.js
@@ -8,115 +8,6 @@
 
 // after script has been loaded, run test code
 window.onload = function() {
-// function insertHeap(heap, n, fn = function(a, b) { return a - b; }) {
-//     heap.push(n);
-
-//     goUp(heap, heap.length - 1, fn);
-// }
-
-// function goUp(heap, i, fn) {
-//     var parent = Math.floor((i - 1) / 2);
-
-//     while (parent >= 0) {
-//         if (fn(heap[parent], heap[i]) > 0) {
-//             var tmp = heap[parent];
-//             heap[parent] = heap[i];
-//             heap[i] = tmp;
-//         }
-//         i = parent;
-//         parent = Math.floor((i - 1) / 2);
-//     }
-// }
-
-// function goDown(nums, i, max, fn) {
-//     var tmp, swapIdx;
-//     var leftChild = 2 * i + 1;
-//     var rightChild = 2 * i + 2;
-
-//     while (leftChild <= max) {
-//         if (rightChild > max) {
-//             if (fn(nums[leftChild], nums[i]) < 0) {
-//                 swapIdx = leftChild;
-//             }
-//         } else {
-//             if (fn(nums[leftChild], nums[i]) < 0 && fn(nums[rightChild], nums[i]) < 0) {
-//                 swapIdx = fn(nums[leftChild], nums[rightChild]) < 0 ?
-//                     leftChild : rightChild;
-//             } else if (fn(nums[leftChild], nums[i]) < 0) {
-//                 swapIdx = leftChild;
-//             } else if(fn(nums[rightChild], nums[i]) < 0) {
-//                 swapIdx = rightChild;
-//             }
-//         }
-//         if (swapIdx !== undefined) {
-//             tmp = nums[swapIdx];
-//             nums[swapIdx] = nums[i];
-//             nums[i] = tmp;
-//             i = swapIdx;
-//             leftChild = 2 * i + 1;
-//             rightChild = 2 * i + 2;
-//             swapIdx = undefined;
-//         } else  {
-//             break;
-//         }
-//     }
-// }
-
-// function popHeap(nums, fn = function(a, b) { return a - b; }) {
-//     if (nums.length === 1) {
-//         return nums.pop();
-//     }
-
-//     var r = nums[0];
-//     nums[0] = nums.pop();
-//     goDown(nums, 0, nums.length - 1, fn);
-
-//     return r;
-// }
-
-// function cmp (a, b) {
-//     return a.localeCompare(b);
-// }
-
-var findItinerary = function(tickets) {
-    var res = [];
-    var map = {};
-
-    for (var i = 0, len = tickets.length; i < len; i++) {
-        if (map[tickets[i][0]] === undefined) {
-            map[tickets[i][0]] = [tickets[i][1]];
-        } else {
-            insertHeap(map[tickets[i][0]], tickets[i][1], cmp);
-        }
-    }
-
-    search('JFK', 0, len, res, map);
-
-    return res; 
-};
-
-function search(cur, idx, max, oneway, map) {
-    if (idx === max) {
-        oneway.push(cur);
-        return true;
-    }
-
-    var next;
-    for(var i = 0, len = (map[cur] || []).length; i < len; i++) {
-        oneway.push(cur);
-        next = popHeap(map[cur], cmp);
-        if (search(next, idx + 1, max, oneway, map)) {
-            return true;
-        }
-        oneway.pop(cur);
-        insertHeap(map[cur], next, cmp);
-    }
-}
-
-// var tickets = [["AXA","EZE"],["EZE","AUA"],["ADL","JFK"],["ADL","TIA"],["AUA","AXA"],["EZE","TIA"],["EZE","TIA"],["AXA","EZE"],["EZE","ADL"],["ANU","EZE"],["TIA","EZE"],["JFK","ADL"],["AUA","JFK"],["JFK","EZE"],["EZE","ANU"],["ADL","AUA"],["ANU","AXA"],["AXA","ADL"],["AUA","JFK"],["EZE","ADL"],["ANU","TIA"],["AUA","JFK"],["TIA","JFK"],["EZE","AUA"],["AXA","EZE"],["AUA","ANU"],["ADL","AXA"],["EZE","ADL"],["AUA","ANU"],["AXA","EZE"],["TIA","AUA"],["AXA","EZE"],["AUA","SYD"],["ADL","JFK"],["EZE","AUA"],["ADL","ANU"],["AUA","TIA"],["ADL","EZE"],["TIA","JFK"],["AXA","ANU"],["JFK","AXA"],["JFK","ADL"],["ADL","EZE"],["AXA","TIA"],["JFK","AUA"],["ADL","EZE"],["JFK","ADL"],["ADL","AXA"],["TIA","AUA"],["AXA","JFK"],["ADL","AUA"],["TIA","JFK"],["JFK","ADL"],["JFK","ADL"],["ANU","AXA"],["TIA","AXA"],["EZE","JFK"],["EZE","AXA"],["ADL","TIA"],["JFK","AUA"],["TIA","EZE"],["EZE","ADL"],["JFK","ANU"],["TIA","AUA"],["EZE","ADL"],["ADL","JFK"],["ANU","AXA"],["AUA","AXA"],["ANU","EZE"],["ADL","AXA"],["ANU","AXA"],["TIA","ADL"],["JFK","ADL"],["JFK","TIA"],["AUA","ADL"],["AUA","TIA"],["TIA","JFK"],["EZE","JFK"],["AUA","ADL"],["ADL","AUA"],["EZE","ANU"],["ADL","ANU"],["AUA","AXA"],["AXA","TIA"],["AXA","TIA"],["ADL","AXA"],["EZE","AXA"],["AXA","JFK"],["JFK","AUA"],["ANU","ADL"],["AXA","TIA"],["ANU","AUA"],["JFK","EZE"],["AXA","ADL"],["TIA","EZE"],["JFK","AXA"],["AXA","ADL"],["EZE","AUA"],["AXA","ANU"],["ADL","EZE"],["AUA","EZE"]];
-var tickets = [["JFK","SFO"],["JFK","ATL"],["SFO","ATL"],["ATL","JFK"],["ATL","SFO"]];
-var r = findItinerary(tickets);
-console.log(r);
 
 // var ladderLength = function(beginWord, endWord, wordList) {
 //     var startIdx = wordList.indexOf(beginWord);
@@ -211,9 +102,54 @@ console.log(r);
 // console.log(r);
 
 // var maximalSquare = function(matrix) {
+//     var imax = matrix.length;
+//     if (imax === 0) {
+//         return 0;
+//     }
+
+//     var dp = {};
+//     var next;
+//     var jmax = matrix[0].length;
+//     var i, j;
+//     var flag = false;
+
+//     for (i = 0; i < imax; i++) {
+//         for (j = 0; j < jmax; j++) {
+//             if (matrix[i][j] === '1') {
+//                 dp[[i, j]] = true;
+//                 flag = true;
+//             }
+//         }
+//     }
+
+//     if (flag === false) {
+//         return 0;
+//     }
+
+//     var count = 1;
+//     var key, tmp;
+
+//     while (true) {
+//         next = {};
+//         flag = false
+//         for (key in dp) {
+//             tmp = key.split(',');
+//             i = Number(tmp[0]);
+//             j = Number(tmp[1]);
+//             if (dp[[i + 1, j]] && dp[[i, j + 1]] && dp[[i + 1, j + 1]]) {
+//                 next[[i, j]] = true;
+//                 flag = true;
+//             }
+//         }
+//         if (!flag) {
+//             return count * count;
+//         }
+//         dp = next;
+//         count++;
+//     }
 // };
 
-// var A = [["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]];
+// var A = [["0"]];
 
 // var r = maximalSquare(A);
 // console.log(r);
