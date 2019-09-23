3d57c9eb1b99d22f710c0fa2b2d225bcea8cbbcf
zhurui66 <cdzhurui8@jd.com>
2019-09-23 18:01:49 +0800

    Update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // dynamically import algorithm
-var js = document.createElement('script');
-js.src = 'maxheap.js';
-document.body.appendChild(js);
+// var js = document.createElement('script');
+// js.src = 'maxheap.js';
+// document.body.appendChild(js);
 var js1 = document.createElement('script');
 js1.src = 'arraytree.js';
 document.body.appendChild(js1);
@@ -16,10 +16,41 @@ document.body.appendChild(js1);
 window.onload = function() {
 
 var trap = function(height) {
-    
+	var count = 0;
+
+    var bottomCount = trapBottom(height);
+    while (height.length > 0) {
+    	count += bottomCount;
+    	bottomCount = trapBottom(height);
+    }
+
+    return count;
 };
 
-var r = trap([0,1,0,2,1,0,1,3,2,1,2,1]);
+function trapBottom(height) {
+	var count = 0;
+	var i = 0;
+	var j;
+	var len = height.length;
+	while (height[i] === 0 && i < len) {
+		height.shift();
+	}
+	var j = height.length - 1;
+	while(height[j] === 0 && j >= 0) {
+		height.pop();
+		j--;
+	}
+	for (i = 0, len = height.length; i < len; i++) {
+		if (height[i] === 0) {
+			count++;
+		} else {
+			height[i]--;
+		}
+	}
+	return count;
+}
+
+var r = trap([4,2,3]);
 console.log(r);
 
 };
