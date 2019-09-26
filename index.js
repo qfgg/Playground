0e08161fbeed9b56ead10c31fa7bf72899c7f9d8
zhurui66 <cdzhurui8@jd.com>
2019-09-26 18:33:29 +0800

    Update

--- index.js
+++ index.js
@@ -15,19 +15,38 @@ document.body.appendChild(js1);
 // after script has been loaded, run test code
 window.onload = function() {
 
-var leastInterval = function(tasks, n) {
-	var taskCount = {};
-    for (var i = 0, len = tasks.length; i < len; i++) {
-    	if (taskCount[tasks[i]] === undefined) {
-    		taskCount[tasks[i]] = 1;
-    	} else {
-    		taskCount[tasks[i]]++;
-    	}
-    }
+var rotate = function(matrix) {
+	var size = matrix.length;
+	var i = 0;
+	while (size - i - i > 1) {
+		rotateLayer(matrix, i, i, size - i - i - 1);
+		i++;
+	}
 };
 
-var tasks = ["A","A","A","B","B","B"], n = 2
-var r = leastInterval(tasks, n);
+function rotateLayer(matrix, x, y, step) {
+	var tmp;
+
+	for (var i = 0; i < step - x; i++) {
+		tmp = matrix[y][x + i];
+		matrix[y][x + i] = matrix[y + step - i][x];
+		matrix[y + step - i][x] = matrix[y + step][x + step - i];
+		matrix[y + step][x + step - i] = matrix[y + i][x + step];
+		matrix[y + i][x + step] = tmp;
+	}
+}
+var r =[
+  [7,4,1],
+  [8,5,2],
+  [9,6,3]
+];
+// var r = [
+//   [ 5, 1, 9,11],
+//   [ 2, 4, 8,10],
+//   [13, 3, 6, 7],
+//   [15,14,12,16]
+// ];
+rotate(r);
 console.log(r);
 
 };
