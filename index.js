9574bfcba9a9041b0779e7b4e9976d653d57f132
zhurui66 <cdzhurui8@jd.com>
2019-11-14 20:59:51 +0800

    Update notes

--- index.js
+++ index.js
@@ -11,38 +11,18 @@
 // var js1 = document.createElement('script');
 // js1.src = 'quicksort.js';
 // document.body.appendChild(js1);
+var js1 = document.createElement('script');
+js1.src = 'list.js';
+document.body.appendChild(js1);
 
 // after script has been loaded, run test code
 window.onload = function() {
 
-var multiply = function(num1, num2) {
-	
+var addTwoNumbers = function() {
 };
 
-<<<<<<< HEAD
-function rotateLayer(matrix, x, y, step) {
-	var tmp;
-
-	for (var i = 0; i < step; i++) {
-		tmp = matrix[y][x + i];
-		matrix[y][x + i] = matrix[y + step - i][x];
-		matrix[y + step - i][x] = matrix[y + step][x + step - i];
-		matrix[y + step][x + step - i] = matrix[y + i][x + step];
-		matrix[y + i][x + step] = tmp;
-	}
-}
-var r =[
-  [ 5, 1, 9,11],
-  [ 2, 4, 8,10],
-  [13, 3, 6, 7],
-  [15,14,12,16]
-];
-
-rotate(r);
-=======
-var r = multiply();
->>>>>>> fa55ad94f9e94fd81a0ed12cbd033637472675e9
-console.log(r);
+var l = addTwoNumbers();
+console.log(l);
 
 };
 
