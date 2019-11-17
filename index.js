3703d0099145cbd107148498dac3e6fb15c261ab
zhurui <zhurui@navercorp.com>
2019-11-17 21:59:45 +0800

    Update

--- index.js
+++ index.js
@@ -15,34 +15,40 @@
 // after script has been loaded, run test code
 window.onload = function() {
 
-var multiply = function(num1, num2) {
-	
-};
 
-<<<<<<< HEAD
-function rotateLayer(matrix, x, y, step) {
-	var tmp;
+function threeProductSuggestions(numProducts, repository, customerQuery)
+{
+    len = customerQuery.length;
+    if (len < 2) {
+        return [];
+    }
 
-	for (var i = 0; i < step; i++) {
-		tmp = matrix[y][x + i];
-		matrix[y][x + i] = matrix[y + step - i][x];
-		matrix[y + step - i][x] = matrix[y + step][x + step - i];
-		matrix[y + step][x + step - i] = matrix[y + i][x + step];
-		matrix[y + i][x + step] = tmp;
-	}
+    var lowerQuery = customerQuery.toLowerCase();
+    var substr;
+    var i;
+    var j;
+    var result = [];
+    repository.sort(function(a, b) {
+        return a.localeCompare(b);
+    });
+    
+    var tmpArr;
+    for (i = 2; i <= len; i++) {
+        tmpArr = [];
+        substr = lowerQuery.slice(0, i);
+        for (j = 0; j < numProducts; j++) {
+            if (repository[j].toLowerCase().indexOf(substr) !== -1) {
+                tmpArr.push(repository[j]);
+            }
+        }
+        result.push(tmpArr.slice(0, 3));
+    }
+    
+    return result;
 }
-var r =[
-  [ 5, 1, 9,11],
-  [ 2, 4, 8,10],
-  [13, 3, 6, 7],
-  [15,14,12,16]
-];
 
-rotate(r);
-=======
-var r = multiply();
->>>>>>> fa55ad94f9e94fd81a0ed12cbd033637472675e9
+
+var r = threeProductSuggestions(5,['bags', 'baggage', 'banner', 'box', 'cloths'], 'bags');
 console.log(r);
-
 };
 
