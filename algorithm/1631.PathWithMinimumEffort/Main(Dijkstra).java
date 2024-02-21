83e92a74800bfc2806285357de28ec3166efeea3
ray <rayjoo333@gmail.com>
2024-02-21 08:45:59 -0800

    1631. Path With Minimum Effort

--- algorithm/1631.PathWithMinimumEffort/Main(Dijkstra).java
+++ algorithm/1631.PathWithMinimumEffort/Main(Dijkstra).java
@@ -4,58 +4,37 @@ import java.util.*;
 public class Main {
     public static int minimumEffortPath(int[][] heights) {
         int rn = heights.length, cn = heights[0].length;
-        int[][] max = new int[rn][cn];
-        for (int[] row: max) {
+        int[] dir = new int[]{-1,0,1,0,-1};
+        int[][] dist = new int[rn][cn];
+        boolean[][] seen = new boolean[rn][cn];
+        for (int[] row : dist) {
             Arrays.fill(row, Integer.MAX_VALUE);
         }
-        max[0][0] = 0;
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> -a[2] + b[2]);
+        dist[0][0] = 0;
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[2] - b[2]);
         pq.add(new int[]{0,0,0});
         int[] cur;
-        int nextMax;
         while (!pq.isEmpty()) {
             cur = pq.poll();
-            if (max[cur[0]][cur[1]] != max[cur[0]][cur[1]]) { // remove deprecated value
+            int r = cur[0], c = cur[1], d = cur[2];
+            if (seen[r][c]) { // remove deprecated value
                 continue;
             }
-            max[cur[0]][cur[1]] = -max[cur[0]][cur[1]];
-            if (cur[0] == rn - 1 && cur[1] == cn - 1) {
+            if (r == rn - 1 && c == cn - 1) {
                 break;
             }
-            if (cur[0] > 0) {
-                nextMax = Math.max(max[cur[0]][cur[1]], Math.abs(heights[cur[0]][cur[1]] - heights[cur[0] - 1][cur[1]]));
-                if (max[cur[0] - 1][cur[1]] == Integer.MAX_VALUE ||
-                        (max[cur[0] - 1][cur[1]] < 0 && nextMax < -max[cur[0] - 1][cur[1]])) {
-                    max[cur[0] - 1][cur[1]] = -nextMax;
-                    pq.add(new int[]{cur[0] - 1, cur[1], -nextMax});
-                }
-            }
-            if (cur[1] > 0) {
-                nextMax = Math.max(max[cur[0]][cur[1]], Math.abs(heights[cur[0]][cur[1]] - heights[cur[0]][cur[1] - 1]));
-                if (max[cur[0]][cur[1] - 1] == Integer.MAX_VALUE ||
-                        (max[cur[0]][cur[1] - 1] < 0 && nextMax < -max[cur[0]][cur[1] - 1])) {
-                    max[cur[0]][cur[1] - 1] = -nextMax;
-                    pq.add(new int[]{cur[0], cur[1] - 1, -nextMax});
-                }
-            }
-            if (cur[0] < rn - 1) {
-                nextMax = Math.max(max[cur[0]][cur[1]], Math.abs(heights[cur[0]][cur[1]] - heights[cur[0] + 1][cur[1]]));
-                if (max[cur[0] + 1][cur[1]] == Integer.MAX_VALUE ||
-                        (max[cur[0] + 1][cur[1]] < 0 && nextMax < -max[cur[0] + 1][cur[1]])) {
-                    max[cur[0] + 1][cur[1]] = -nextMax;
-                    pq.add(new int[]{cur[0] + 1, cur[1], -nextMax});
-                }
-            }
-            if (cur[1] < cn - 1) {
-                nextMax = Math.max(max[cur[0]][cur[1]], Math.abs(heights[cur[0]][cur[1]] - heights[cur[0]][cur[1] + 1]));
-                if (max[cur[0]][cur[1] + 1] == Integer.MAX_VALUE ||
-                        (max[cur[0]][cur[1] + 1] < 0 && nextMax < -max[cur[0]][cur[1] + 1])) {
-                    max[cur[0]][cur[1] + 1] = -nextMax;
-                    pq.add(new int[]{cur[0], cur[1] + 1, -nextMax});
+            int pos, nr, nc;
+            seen[r][c] = true;
+            for (pos = 0; pos < 4; pos++) {
+                nr = r + dir[pos];
+                nc = c + dir[pos + 1];
+                if (nr >= 0 && nc >= 0 && nr < rn && nc < cn && Math.max(d, Math.abs(heights[r][c] - heights[nr][nc])) < dist[nr][nc]) {
+                    dist[nr][nc] = Math.max(d, Math.abs(heights[r][c] - heights[nr][nc]));
+                    pq.add(new int[]{nr, nc, dist[nr][nc]});
                 }
             }
         }
-        return max[rn - 1][cn - 1];
+        return dist[rn - 1][cn - 1];
     }
     public static void main(String[] args) {
         int[][] heights = new int[][]{
